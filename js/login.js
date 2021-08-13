0c77802b5adac31182455208e60577ef43dec978
michufeig <feigmichelle@gmail.com>
2021-08-12 21:43:05 -0300

    rehaciendo entrega 1

--- js/login.js
+++ js/login.js
@@ -1,10 +1,10 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-    document.getElementById("sendUsername").onclick = function(e){
+// document.addEventListener("DOMContentLoaded", function(e){
+//     document.getElementById("sendUsername").onclick = function(e){
 
-        window.location.href = "./index.html";
-    }
+//         window.location.href = "./index.html";
+//     }
 
-});
+// });
