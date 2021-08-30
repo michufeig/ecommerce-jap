43ecd7b8897a6b2a23b2696ede2ebb4b6004810d
michufeig <feigmichelle@gmail.com>
2021-08-29 23:50:39 -0300

    vuelvo a lo que esta bien

--- js/products.js
+++ js/products.js
@@ -1,7 +1,6 @@
 // ENTREGA 1. MUESTRO LISTA DE PRODUCTOS
 
 document.addEventListener("DOMContentLoaded", function(e){
-    
     fetch(PRODUCTS_URL)
         .then(info => info.json())         //pone en variable info q toma de la url, convierto la var en json, llamo a esto data
         .then(data => {   
@@ -9,36 +8,31 @@ document.addEventListener("DOMContentLoaded", function(e){
             // let i = 0;
             // while(i < data.length){
 
-            function productsList() {
-                for(let i = 0; i < data.length; i++){                    
-                    let nombre = data[i].name;
-                    let descripcion = data[i].description;
-                    let costo = data[i].cost;
-                    let imagen = data[i].imgSrc;
-    
-                    let productos = "";
-    
-                    productos += `
-                        <div class="row">
-                            <div class="col-3">
-                            <img src="` + imagen + `" alt="` + descripcion + `" class="img-thumbnail"> 
-                            </div>
-                            <div class="col">
-                                <div class="d-flex w-100 justify-content-between">
-                                    <h4 class="mb-1">`+ nombre +`</h4>
-                                    <small class="text-muted"> USD ` + costo + `</small>
-                                </div>
-                                <p class="mb-1">` + descripcion + `</p>
-                            </div>
-                        </div>
-                    `     
-                    document.getElementById("listaProductos").innerHTML += productos;
-                    // i = i + 1
-                }
-            }
+            for(let i = 0; i < data.length; i++){                    
+                let nombre = data[i].name;
+                let descripcion = data[i].description;
+                let costo = data[i].cost;
+                let imagen = data[i].imgSrc;
 
-            productsList();
-            
+                let productos = "";
+
+                productos += `
+                    <div class="row">
+                        <div class="col-3">
+                        <img src="` + imagen + `" alt="` + descripcion + `" class="img-thumbnail"> 
+                        </div>
+                        <div class="col">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h4 class="mb-1">`+ nombre +`</h4>
+                                <small class="text-muted"> USD ` + costo + `</small>
+                            </div>
+                            <p class="mb-1">` + descripcion + `</p>
+                        </div>
+                    </div>
+                `     
+                document.getElementById("listaProductos").innerHTML += productos;
+                // i = i + 1
+            }
         });
 
         //ENTREGA 2. FILTRAR A PARTIR DE MINIMO Y MAXIMO INTRODUCIDO 
@@ -85,7 +79,7 @@ document.addEventListener("DOMContentLoaded", function(e){
                     }
 
                 }
-
+                
             })
         }
 
